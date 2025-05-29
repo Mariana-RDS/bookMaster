9194886c8fc8e51ca12afe9455949e3958a07aed
Mariana Santos <mrds.profissional@gmail.com>
2025-05-29 19:30:30 -0300

    aula 26 de Maio

--- bookmaster/src/main/java/com/br/ifpe/edu/bookmaster/controller/helloController.java
+++ bookmaster/src/main/java/com/br/ifpe/edu/bookmaster/controller/helloController.java
@@ -11,4 +11,21 @@ public class helloController {
         return "Hello, World";
     }
 
+    
+    // http://localhost:8080/api/param/Mari
+    // http://localhost:8080/api/param/Mari/Santos
+
+    @GetMapping("param/{nome}/{sobrenome}")
+    public String helloParam(@PathVariable String nome, @PathVariable String sobrenome){
+        return ("hello, " + nome +" "+ sobrenome);
+    }
+
+    // http://localhost:8080/api/paramNome?nome=Mari
+    // http://localhost:8080/api/paramNome?nome=mariana&sobrenome=santos
+
+    @GetMapping("paramNome")
+    public String helloParamNome(@RequestParam String nome, @RequestParam String sobrenome){
+        return ("hello, " + nome +" "+ sobrenome);
+    }
+
 }
